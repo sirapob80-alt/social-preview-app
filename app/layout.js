6877ee40f4155d221563f76ce5ce0497c94f2adb
sirapob80-alt <sirapob80@gmail.com>
2026-09-11 18:38:10 +0700

    Add app layout

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'LinkRoom',description:'Temporary chat rooms for sharing social links'};
+export default function RootLayout({children}){return <html lang="th"><body>{children}</body></html>}
